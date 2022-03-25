2970c46f0793f175ea6d91f4ec1e943fdacac6f3
joake <joakim@vev.design>
2022-03-25 14:06:59 +0100

    added new background assets

--- Ball2.gd
+++ Ball2.gd
@@ -1,7 +1,8 @@
 extends KinematicBody2D
 
-export var velocity = Vector2(0, 200);
+export var velocity = Vector2(0, 150);
 var screen_size;
+export var GRAVITY_EFFECT = 5;
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
@@ -18,10 +19,11 @@ func _ready():
 #	pass
 
 func _physics_process(delta):
+	velocity.y += GRAVITY_EFFECT;
 	var collision_info = move_and_collide(velocity * delta);
-	print(collision_info)
 	if collision_info:
-		velocity = velocity.bounce(collision_info.normal)
-		velocity.x *= 1.1
-		velocity.y *= 1.1
+		print(collision_info)
+		velocity = velocity.bounce(collision_info.normal);
+		if (collision_info.get_instance_id() == 1208 && Input.is_action_pressed("ui_left")):
+			velocity.y *= 1.1
 	
